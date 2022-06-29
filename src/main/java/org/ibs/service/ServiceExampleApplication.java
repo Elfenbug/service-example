589992f1d18108242a40b0244d982745f0127c5b
MKashapov <MKashapov@ibs.ru>
2022-06-29 15:31:54 +0500

    Initial commit

---
+++ src/main/java/org/ibs/service/ServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package org.ibs.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceExampleApplication.class, args);
+    }
+
+}
